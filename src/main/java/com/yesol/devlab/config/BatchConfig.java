644fb037caeaa8a5324ba071fea8c8b082038690
Yesol <flowerbud94@gmail.com>
2024-11-01 16:57:59 +0900

    spring batch 구현중

---
+++ src/main/java/com/yesol/devlab/config/BatchConfig.java
@@ -0,0 +1,11 @@
+package com.yesol.devlab.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+
+}
